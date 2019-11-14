00abcb7ea77d2306a6cb202c4c30e105e48c0f3b
Vaecrid <57478439+Vaecrid@users.noreply.github.com>
2019-11-14 18:58:53 +0100

    Add files via upload

---
+++ LDR_Lindar.ino
@@ -0,0 +1,20 @@
+const int LEDPin = 13;
+const int LDRPin = A1;
+const int threshold = 100;
+ 
+void setup() {
+  
+   pinMode(LEDPin, OUTPUT);
+   pinMode(LDRPin, INPUT);
+}
+ 
+void loop() {
+  
+   int input = analogRead(LDRPin);
+   if (input > threshold) {
+      digitalWrite(LEDPin, HIGH);
+   }
+   else {
+      digitalWrite(LEDPin, LOW);
+   }
+}
